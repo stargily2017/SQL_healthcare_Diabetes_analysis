79d2a81517407b6aa9bea7473c0d5f101f9cb1bc
stargily2017 <117419179+stargily2017@users.noreply.github.com>
2025-11-10 23:08:47 -0500

    Update diabetes_healthcareproject.sql

--- diabetes_healthcareproject.sql
+++ diabetes_healthcareproject.sql
@@ -1,13 +1,13 @@
 USE patient;
 SELECT * FROM health;
 
-/*
-#1. What medical specialties are doing the most procedures on average? 
- The hospital is reviewing its laboratory procedures to identify the average processing times and common patterns.
-I used the Distinct keyword to retrieve unique values from the data; it also eliminates duplicates.
-The top average lab procedure 3.5 is in surgery-thoracic. Depending o the patient count, the top average is in Cardiology procedures. 
-The patient spent more than 5 days in the hospital for cardiovascular-thoracic surgery.
-/* 
+
+---#1. What medical specialties are doing the most procedures on average? 
+--The hospital is reviewing its laboratory procedures to identify the average processing times and common patterns.
+--I used the Distinct keyword to retrieve unique values from the data; it also eliminates duplicates.
+--The top average lab procedure 3.5 is in surgery-thoracic. Depending o the patient count, the top average is in Cardiology procedures. 
+--The patient spent more than 5 days in the hospital for cardiovascular-thoracic surgery.
+
 
 SELECT distinct(medical_specialty), 
 round(avg(num_procedures),1)AS avg_procedures,
@@ -20,13 +20,13 @@ and avg(num_procedures) >2.5
 order by avg_procedures DESC;
 
 ---------------------------------------------
-/* 
-#2.Hyperglycemia has a significant role in hospital admissions.
-I found out which common medications patients use —are they the popular ones? 
-Also, which age group uses the top-ranked medication?
-I used CTEs to break the querries into small.
-Insulin, Metformin, Glypizide, and Glyburide are the most commonly used diabetes medications.
-/*
+
+--#2.Hyperglycemia has a significant role in hospital admissions.
+--I found out which common medications patients use —are they the popular ones? 
+--Also, which age group uses the top-ranked medication?
+--I used CTEs to break the querries into small.
+--Insulin, Metformin, Glypizide, and Glyburide are the most commonly used diabetes medications.
+
 
 WITH long_health_cte AS (
 SELECT patient_nbr, 'metformin' AS medication, metformin AS med_usage FROM patient.health
@@ -76,12 +76,12 @@ WHERE med_usage NOT IN ('No')
 GROUP BY medication, age
 ORDER BY total_uses DESC, medication_rank;
 --------------------------------------------------------------------------------------
-/*
-#3.Potential strategies to cut readmission costs for diabetes.
-Based on race: Hospitals could benefit from more personalized diabetes management plans, 
-especially by emphasizing regular hyperglycemia monitoring to predict potential readmissions.
-I used the CASE function to allow if-then-else logic within a query.
-/*
+
+--#3.Potential strategies to cut readmission costs for diabetes.
+--Based on race: Hospitals could benefit from more personalized diabetes management plans, 
+--especially by emphasizing regular hyperglycemia monitoring to predict potential readmissions.
+--I used the CASE function to allow if-then-else logic within a query.
+
 
 SELECT d.age, round(avg(h.num_lab_procedures)) AS avg_labprocedures,
 SUM(CASE when readmitted = '<30' then 1 else 0 end)
@@ -91,10 +91,10 @@ JOIN demographics AS d ON h.patient_nbr = d.patient_nbr
 GROUP BY d.age
 ORDER BY readmitted_within30_days DESC;
 ---------------------------------------------
-/*
-Based on age: Age is a crucial factor in hospital readmission rates for patients with diabetes.
-[0-30], [30-60], and [60-100]. Average lab procedures were similar across all ages.
-/*
+
+--Based on age: Age is a crucial factor in hospital readmission rates for patients with diabetes.
+--[0-30], [30-60], and [60-100]. Average lab procedures were similar across all ages.
+
 
 SELECT d.age, round(avg(h.num_lab_procedures)) AS avg_labprocedures,
 SUM(CASE when readmitted = '<30' then 1 else 0 end)
@@ -105,10 +105,10 @@ GROUP BY d.age
 ORDER BY readmitted_within30_days DESC;
 -------------------------------------------------------------------
 
-/*
-Based on gender: (Female has higher readmission rates despite receiving fewer medications than males.)
-Females were readmitted within 30 days than the males, 
-/*
+
+--Based on gender: (Female has higher readmission rates despite receiving fewer medications than males.)
+--Females were readmitted within 30 days than the males, 
+
 SELECT d.gender AS gender,
 max(num_medications) AS medications,
 round(avg(time_in_hospital),2) AS avg_time_in_days,
@@ -122,12 +122,12 @@ demographics AS d ON h.patient_nbr = d.patient_nbr
 GROUP BY d.gender;
 ------------------------------------------------------------
 
-/*
-#4. Patients spent 1 to 14 days in the hospital during their treatment 
-Time is an essential factor for hospitalization.
-I noticed the bar chart shows the length of stay ranged from 1 to 14 days. Here, 
-I introduce the CAST function in SQL, which converts one data type to another.
-/*
+
+--#4. Patients spent 1 to 14 days in the hospital during their treatment 
+--Time is an essential factor for hospitalization.
+--I noticed the bar chart shows the length of stay ranged from 1 to 14 days. Here, 
+--I introduce the CAST function in SQL, which converts one data type to another.
+
 SELECT round(CAST(time_in_hospital AS Decimal), 1) AS Time_in_days,
 count(*) AS patients_count,
 rpad('', count(*)/200,'*') AS bar
@@ -135,11 +135,11 @@ FROM health
 group by Time_in_days
 order by Time_in_days Desc;
 ----------------------------------------------------------
-/*
-#5. Find the patient's data from the medical records at any time
- I used the CONCAT function in this query. It's a string function 
- that combines two or more string values into a single string.
- /*
+
+--#5. Find the patient's data from the medical records at any time
+ --I used the CONCAT function in this query. It's a string function 
+ --that combines two or more string values into a single string.
+ 
  
 SELECT 
     CONCAT('Patient ',
